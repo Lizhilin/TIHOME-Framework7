1eb1d19c7549b03029dfb4f94ce70f593b8cfde0
Lizhilin <LZL61219@163.com>
2016-10-08 15:38:50 +0800

    标签排序待续==

--- www/js/UI.js
+++ www/js/UI.js
@@ -28,6 +28,8 @@ var liObj = function () {
     this.y;
     this.eX;
     this.eY;
+    this.inX;
+    this.inY;
     this.opacity;
 }
 liObj.prototype.init = function () {
@@ -36,42 +38,100 @@ liObj.prototype.init = function () {
     this.move();
 }
 liObj.prototype.start = function () {
-    this.el.addEventListener('touchstart', function (event) {
+    var _this = this;
+    var _thisEL = this.el;
+    _thisEL.addEventListener('touchstart', function (event) {
         var event = event || window.event;
         event.preventDefault();
+        _this.eX = event.touches[0].clientX;
+        _this.eY = event.touches[0].clientY;
+
         setTimeout(function () {
-            console.log( event.target)
-        },500)
-        this.eX = event.touches[0].clientX;
-        this.eY = event.touches[0].clientY;
+            if($(event.target).hasClass('active-state')){
+                _this.x = _thisEL.offsetLeft;
+                _this.y = _thisEL.offsetTop;
+                _this.inX = _this.eX - _this.x;
+                _this.inY = _this.eY - _this.y;
+                $(event.target).clone().addClass('clone').appendTo($('.tab-box .showbox ul:first-of-type'));
+                _thisEL.style.visibility = 'hidden';//隐藏本体
+                $('li.clone').css({'left':_this.x, 'top':_this.y});
+            }
+        },500);
+
         console.log('touchstart')
     },false);
 }
 liObj.prototype.end = function () {
-    this.el.addEventListener('touchend', function (event) {
+    var _this = this;
+    var _thisEL = this.el;
+    _thisEL.addEventListener('touchend', function (event) {
         var event = event || window.event;
         event.preventDefault();
+        $('li.clone').remove();
+        _thisEL.style.visibility = 'visible';
         console.log('touchend')
     },false);
 }
 liObj.prototype.move = function () {
-    this.el.addEventListener('touchmove', function (event) {
+    var _this = this;
+    var _thisEL = this.el;
+    _thisEL.addEventListener('touchmove', function (event) {
         var event = event || window.event;
         event.preventDefault();
-        //X = event.touches[0].clientX - touchInnerX;
-        //Y = event.touches[0].clientY - touchInnerY;
+        if($(event.target).siblings().hasClass('clone')){
+            $('li.clone').css({
+                'left': event.touches[0].clientX - _this.inX,
+                'top' : event.touches[0].clientY - _this.inY
+            })
+        }
+
         console.log('touchmove')
     },false);
 }
 
 function draglist($_ul){
+    var lisXY = [];
     $_ul.children().each(function (index, ele) {
+        var offsetLeft=ele.offsetLeft, offsetTop=ele.offsetTop;
         lis[index] = new liObj();
         lis[index].el = ele;
         lis[index].init();
-    })
+        lis[index].x = offsetLeft;
+        lis[index].y = offsetTop;
+        lisXY.push({x:offsetLeft, y:offsetTop})
+        $(ele).css({
+            'left': offsetLeft,
+            'top' : offsetTop,
+        });
+    });
+    $_ul.height($_ul.height());
+    $_ul.children().css('position','absolute');
+    console.log(lisXY)
+    var lis_X = [], lis_Y = [];
+    for(var i in lisXY){
+        lis_X.push(lisXY[i].x);
+        lis_Y.push(lisXY[i].y);
+    }
+    console.log(arrayUnique(lis_X),arrayUnique(lis_Y))
 }
 
 draglist($('.tab-box .showbox ul:first-of-type'));
+
+
+
+//去重
+function arrayUnique(arrs){
+    var newArrays = [];
+    var hash = {};
+    if(arrs.length > 0){
+        for(var i in arrs){
+            if( ! hash[arrs[i]]){
+                hash[arrs[i]]=1;
+                newArrays.push(arrs[i]);
+            }
+        }
+    }
+    return newArrays;
+}
 
 
